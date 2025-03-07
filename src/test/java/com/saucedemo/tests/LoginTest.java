af2261e7bc9cbea64f93cd47bf103e2a6018fa59
Dusan <simic.dusan95@gmail.com>
2025-03-08 00:12:29 +0100

    Initial commit - Selenium Sauce Demo

---
+++ src/test/java/com/saucedemo/tests/LoginTest.java
@@ -0,0 +1,42 @@
+package com.saucedemo.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LoginTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @Test
+    public void testValidLogin() throws InterruptedException {
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        WebElement inventoryPage = driver.findElement(By.className("inventory_list"));
+        Assert.assertTrue(inventoryPage.isDisplayed(), "Login failed!");
+        Thread.sleep(3000);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
