4c19edf956a6aeeb384ed3869b5cd49d35cffa66
Dusan <simic.dusan95@gmail.com>
2025-03-10 22:59:07 +0100

    Implemented Extent Reports, updated the gitignore file

--- src/test/java/com/saucedemo/tests/BaseTest.java
+++ src/test/java/com/saucedemo/tests/BaseTest.java
@@ -1,5 +1,8 @@
 package com.saucedemo.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.saucedemo.reports.ExtentManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -7,7 +10,9 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.FileInputStream;
@@ -15,14 +20,24 @@ import java.io.IOException;
 import java.util.Properties;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
     protected WebDriver driver;
     protected Properties config;
     private static final Logger logger = LogManager.getLogger(BaseTest.class);
+    protected static ExtentReports extent;
+    protected static ExtentTest test;
+
+    @BeforeSuite
+    public void setupExtentReports(){
+        extent = ExtentManager.getInstance();
+    }
 
     @BeforeMethod
     public void setup() throws IOException {
+        test = ExtentManager.createTest(this.getClass().getSimpleName());
+
         logger.info("Launching the test environment...");
         config = new Properties();
         FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
@@ -56,11 +71,23 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+        if(result.getStatus() == ITestResult.FAILURE){
+            test.fail("Test failed: " + result.getThrowable());
+        } else if (result.getStatus() == ITestResult.SUCCESS){
+            test.pass("Test passed");
+        } else {
+            test.skip("Test skipped");
+        }
         if(driver != null){
             logger.info("Closing the browser...");
             driver.quit();
         }
+    }
+
+    @AfterSuite
+    public void flushReports(){
+        ExtentManager.flushReports();
     }
 
 }
