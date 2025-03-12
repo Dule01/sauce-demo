e9f5e818934e5e595299f0a973301fc64ceff933
Dusan <simic.dusan95@gmail.com>
2025-03-12 22:57:04 +0100

    Added data providers functionality

--- src/test/java/com/saucedemo/tests/LoginTest.java
+++ src/test/java/com/saucedemo/tests/LoginTest.java
@@ -2,6 +2,7 @@ package com.saucedemo.tests;
 
 import com.saucedemo.pages.InventoryPage;
 import com.saucedemo.pages.LoginPage;
+import com.saucedemo.utils.DataProviders;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
@@ -9,21 +10,27 @@ import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest{
     private static final Logger logger = LogManager.getLogger(LoginTest.class);
-    @Test
-    public void testValidLogin(){
-        logger.info("Launching test: testValidLogin");
-        test.info("Launching test: testValidLogin");
+
+    @Test(dataProvider = "successfulLoginData", dataProviderClass = DataProviders.class)
+    public void testValidLogin(String username, String password, boolean expectedSuccess){
+        logger.info("Launching test: testValidLogin for the user: " + username);
+        test.info("Launching test: testValidLogin for the user: " + username);
 
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.login("standard_user", "secret_sauce");
+        loginPage.login(username, password);
 
-        logger.info("Logged in user: standard_user");
-        test.info("Logged in user: standard_user");
+        logger.info("Logged in user: " + username);
+        test.info("Logged in user: " + password);
 
         InventoryPage inventoryPage = new InventoryPage(driver);
-        Assert.assertTrue(inventoryPage.isInventoryDisplayed(), "Login failed!");
+        boolean actualSuccess = inventoryPage.isInventoryDisplayed();
 
-        logger.info("Test successful: User is logged in!");
-        test.info("Test successful: User is logged in!");
+        Assert.assertEquals(actualSuccess, expectedSuccess, "Login test failed for the user: " + username);
+
+        if (expectedSuccess) {
+            test.pass("User successfully logged in: " + username);
+        } else {
+            test.fail("Login not successful for the user: " + username);
+        }
     }
 }
---
+++ src/test/java/com/saucedemo/utils/DataProviders.java
@@ -0,0 +1,24 @@
+package com.saucedemo.utils;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviders {
+
+    @DataProvider(name = "loginData")
+    public static Object[][] getLoginData(){
+        return new Object[][]{
+                {"standard_user", "secret_sauce", true},
+                {"locked_out_user", "secret_sauce", false},
+                {"wrong_user", "wrong_pass", false},
+                {"standard_user", "", false},
+                {"", "secret_sauce", false}
+        };
+    }
+
+    @DataProvider(name = "successfulLoginData")
+    public static Object[][] getSuccessfulLoginData(){
+        return new Object[][]{
+                {"standard_user", "secret_sauce", true},
+        };
+    }
+}
